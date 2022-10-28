c57f3785989f59d8fb2be520d99c5bf64ed68265
Curran Duke <cdsonics@yahoo.com>
2022-10-28 02:26:40 -0400

    added car img

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function jump(){
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<40 && blockLeft>-20 && characterTop>=130){
+    if(blockLeft<40 && blockLeft>-40 && characterTop>=150){
         block.style.animation = "none";
         alert("Game Over. score: "+Math.floor(counter/100));
         counter=0;
